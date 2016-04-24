8a4debbb7e6fcd87a070e071c2952fe688d63f13
Dmitry Sizon <dimonsizon@mail.ru>
2016-04-24 20:13:33 +0300

    Added image slider to home page.

--- views/home/home.js
+++ views/home/home.js
@@ -4,5 +4,13 @@ angular.module('app.home', ['ngRoute'])
 
 .controller('HomeCtrl', ['$scope', '$http', '$rootScope', 'appConfig',
     function ($scope, $http, $rootScope, appConfig) {
-        $scope.test = 'Test )))';
+        $scope.sliderImages = [
+            '/images/slider-images/1.jpg',
+            '/images/slider-images/2.jpg',
+            '/images/slider-images/3.jpg',
+            '/images/slider-images/4.jpg',
+            '/images/slider-images/5.jpg',
+            '/images/slider-images/6.jpg',
+            '/images/slider-images/7.jpg'
+        ];
     }]);
